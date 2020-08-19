fd8d691df3e09580c54f1527e7512510e5275e11
19013608 <19013608@LAPTOP-51IQHAOM>
2020-08-19 18:50:42 +0800

    Add, View and Archive Transaction for C206_CaseStudy and Test

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -4,46 +4,195 @@ public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<Food> foodList = new ArrayList<Food>();
-		ArrayList<Toys> toyList = new ArrayList<Toys>();
-		
+		ArrayList<Exchange> exchangeList = new ArrayList<Exchange>();
+		ArrayList<Refund> refundList = new ArrayList<Refund>();
+
 		int option = 0;
-		
-		while (option != 5) {
+
+		while (option != 4) {
 			C206_CaseStudy.menu();
 			option = Helper.readInt("Enter an option > ");
-			
+
 			if (option == 1) {
-				//View all products
+				// View all transactions
+				C206_CaseStudy.viewAllExchange(exchangeList);
+				C206_CaseStudy.viewAllRefund(refundList);
+
 			} else if (option == 2) {
-				//Add products
-				int itemType = Helper.readInt("Enter option to select item type > ");
-				if (itemType == 1) {
-					//Add a Food
-				} else if (itemType == 2) {
-					//Add a Toy
+				// Add transactions
+				C206_CaseStudy.setHeader("ADD");
+				C206_CaseStudy.setHeader("ITEM TYPES");
+				System.out.println("1. Exchange");
+				System.out.println("2. Refund");
+				int transactionType = Helper.readInt("Enter a transaction type > ");
+				if (transactionType == 1) {
+					// Add an exchange
+					Exchange ex = inputExchange();
+					C206_CaseStudy.addExchange(exchangeList, ex);
+
+				} else if (transactionType == 2) {
+					// Add a refund
+					Refund re = inputRefund();
+					C206_CaseStudy.addRefund(refundList, re);
+
+				} else {
+					System.out.println("Invalid type");
 				}
 			} else if (option == 3) {
-				//Delete a product
-			} else if (option == 4) {
-				System.out.println("Bye!");
-			} else {
-				System.out.println("Invalid option!");
+				// Archive a transaction
+				C206_CaseStudy.setHeader("ARCHIVE");
+				C206_CaseStudy.setHeader("ITEM TYPES");
+				System.out.println("1. Exchange");
+				System.out.println("2. Refund");
+				int transactionType = Helper.readInt("Enter a transaction type > ");
+				if (transactionType == 1) {
+					// Add an exchange
+					C206_CaseStudy.archiveExchange(exchangeList);
+
+				} else if (transactionType == 2) {
+					// Add a refund
+					C206_CaseStudy.archiveRefund(refundList);
+
+				} else if (option == 4) {
+					System.out.println("Bye!");
+				} else {
+					System.out.println("Invalid option!");
+				}
 			}
 		}
 	}
-	
+
 	public static void menu() {
 		C206_CaseStudy.setHeader("DISO APP");
-		System.out.println("1. VIEW ALL PRODUCTS");
-		System.out.println("2. ADD A PRODUCT");
-		System.out.println("3. DELETE A PRODUCT");
+		System.out.println("1. VIEW ALL TRANSACTIONS");
+		System.out.println("2. ADD A TRANSACTION");
+		System.out.println("3. ARCHIVE A TRANSACTION");
 		System.out.println("4. QUIT");
 		Helper.line(80, "-");
 	}
+
 	public static void setHeader(String header) {
 		Helper.line(80, "-");
 		System.out.println(header);
 		Helper.line(80, "-");
+	}
 
+	// ================================= Option 1 View=================================
+	public static String retrieveAllExchange(ArrayList<Exchange> exchangeList) {
+		String output = "";
+
+		for (int i = 0; i < exchangeList.size(); i++) {
+			output += String.format("%-10d %-30s %-10.2f\n", exchangeList.get(i).getId(),
+					exchangeList.get(i).getName(), exchangeList.get(i).getPrice());
+		}
+		return output;
+	}
+
+	public static void viewAllExchange(ArrayList<Exchange> exchangeList) {
+		C206_CaseStudy.setHeader("EXCHANGE LIST");
+		String output = String.format("%-10s %-30s %-10s\n", "ID", "NAME", "PRICE");
+		output += retrieveAllExchange(exchangeList);
+		System.out.println(output);
+	}
+
+	public static String retrieveAllRefund(ArrayList<Refund> refundList) {
+		String output = "";
+
+		for (int i = 0; i < refundList.size(); i++) {
+			output += String.format("%-10d %-30s %-10.2f\n", refundList.get(i).getId(),
+					refundList.get(i).getName(), refundList.get(i).getPrice());
+		}
+		return output;
+	}
+
+	public static void viewAllRefund(ArrayList<Refund> refundList) {
+		C206_CaseStudy.setHeader("REFUND LIST");
+		String output = String.format("%-10s %-30s %-10s\n", "ID", "NAME", "PRICE");
+		output += retrieveAllRefund(refundList);
+		System.out.println(output);
+	}
+
+	// ================================= Option 2 Add=================================
+	public static Exchange inputExchange() {
+		int id = Helper.readInt("Enter ID > ");
+		String name = Helper.readString("Enter Name > ");
+		double price = Helper.readDouble("Enter Price > ");
+
+		Exchange ex = new Exchange(id, name, price);
+		return ex;
+
+	}
+
+	public static void addExchange(ArrayList<Exchange> exchangeList, Exchange ex) {
+
+		exchangeList.add(ex);
+		System.out.println("Exchange added");
+	}
+
+	public static Refund inputRefund() {
+		int id = Helper.readInt("Enter ID > ");
+		String name = Helper.readString("Enter Name > ");
+		double price = Helper.readDouble("Enter Price > ");
+
+		Refund re = new Refund(id, name, price);
+		return re;
+
+	}
+
+	public static void addRefund(ArrayList<Refund> refundList, Refund re) {
+
+		refundList.add(re);
+		System.out.println("Refund added");
+	}
+
+	// ================================= Option 3 Archive=================================
+	public static boolean doArchiveExchange(ArrayList<Exchange> exchangeList, int id) {
+		boolean isArchived = false;
+
+		for (int i = 0; i < exchangeList.size(); i++) {
+			if (id == exchangeList.get(i).getId()) {
+				exchangeList.remove(i);
+				isArchived = true;
+			}
+		}
+		return isArchived;
+
+	}
+
+	public static void archiveExchange(ArrayList<Exchange> exchangeList) {
+		C206_CaseStudy.viewAllExchange(exchangeList);
+		int id = Helper.readInt("Enter ID > ");
+		Boolean isArchived = doArchiveExchange(exchangeList, id);
+
+		if (isArchived == false) {
+			System.out.println("Invalid ID");
+		} else {
+			System.out.println("Exchange " + id + " archived");
+		}
+	}
+
+	public static boolean doArchiveRefund(ArrayList<Refund> refundList, int id) {
+		boolean isArchived = false;
+
+		for (int i = 0; i < refundList.size(); i++) {
+			if (id == refundList.get(i).getId()) {
+				refundList.remove(i);
+				isArchived = true;
+			}
+		}
+		return isArchived;
+
+	}
+
+	public static void archiveRefund(ArrayList<Refund> refundList) {
+		C206_CaseStudy.viewAllRefund(refundList);
+		int id = Helper.readInt("Enter ID > ");
+		Boolean isArchived = doArchiveRefund(refundList, id);
+
+		if (isArchived == false) {
+			System.out.println("Invalid ID");
+		} else {
+			System.out.println("Refund " + id + " archived");
+		}
+	}
 }
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,145 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
-
+	private Exchange ex1;
+	private Exchange ex2;
+	private Refund re1;
+	private Refund re2;
+	
+	private ArrayList<Exchange> exchangeList;
+	private ArrayList<Refund> refundList;
+	
+	public C206_CaseStudyTest() {
+		super();
+	}
+	
 	@Before
 	public void setUp() throws Exception {
+		//prepare test data
+		ex1 = new Exchange(1, "Exchange 1", 10);
+		ex2 = new Exchange(2, "Exchange 2", 20);
+		re1 = new Refund(3, "Refund 1", 30);
+		re2 = new Refund(4, "Refund 2", 40);
+		
+		exchangeList = new ArrayList<Exchange>();
+		refundList = new ArrayList<Refund>();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		ex1 = null;
+		ex2 = null;
+		re1 = null;
+		re2 = null;
 	}
 
 	@Test
-	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
+	public void addExchangeTest() {
+		//=============================ADDING EXCHANGE=========================================
+		// Item list is not null, so that can add a new item - boundary
+		assertNotNull("Check if there is valid Exchange arraylist to add to", exchangeList);
+		//Given an empty list, after adding 1 item, the size of the list is 1 - normal
+		//The item just added is as same as the first item of the list
+		C206_CaseStudy.addExchange(exchangeList, ex1);
+		assertEquals("Check that Exchange arraylist size is 1", 1, exchangeList.size());
+		assertSame("Check that Exchange is added", ex1, exchangeList.get(0));
+				
+		//Add another item. test The size of the list is 2? -normal
+		//The item just added is as same as the second item of the list
+		C206_CaseStudy.addExchange(exchangeList, ex2);
+		assertEquals("Check that Exchange arraylist size is 2", 2, exchangeList.size());
+		assertSame("Check that Exchange is added", ex2, exchangeList.get(1));
+	}
+	
+	@Test
+	public void addRefundTest() {
+		//=============================ADDING REFUND============================================
+		// Item list is not null, so that can add a new item - boundary
+		assertNotNull("Test if there is valid Refund arraylist to add to", refundList);
+				
+		//Given an empty list, after adding 1 item, the size of the list is 1 - normal
+		//The item just added is as same as the first item of the list
+		C206_CaseStudy.addRefund(refundList, re1);		
+		assertEquals("Test that Refund arraylist size is 1", 1, refundList.size());
+		assertSame("Test that Refund is added", re1, refundList.get(0));
+				
+		//Add another item. test The size of the list is 2? - normal
+		//The item just added is as same as the second item of the list
+		C206_CaseStudy.addRefund(refundList, re2);
+		assertEquals("Test that Refund arraylist size is 2", 2, refundList.size());
+		assertSame("Test that Refund is added", re2, refundList.get(1));
+	}
+		
+	@Test
+	public void viewAllExchangeTest() {
+		//=============================VIEW ALL EXCHANGE=========================================
+		// Test if Item list is not null but empty -boundary
+		assertNotNull("Test if there is valid Exchange arraylist to retrieve item", exchangeList);
+				
+		//test if the list of Exchange retrieved from the SourceCentre is empty - boundary
+		String allExchange = C206_CaseStudy.retrieveAllExchange(exchangeList);
+		String testOutput = "";
+		assertEquals("Check that ViewAllExchangelist", testOutput, allExchange);
+				
+		//Given an empty list, after adding 2 items, test if the size of the list is 2 - normal
+		C206_CaseStudy.addExchange(exchangeList, ex1);
+		C206_CaseStudy.addExchange(exchangeList, ex2);
+		assertEquals("Test that Exchange arraylist size is 2", 2, exchangeList.size());
+				
+		//test if the expected output string same as the list of Exchange retrieved from the SourceCentre	
+		allExchange = C206_CaseStudy.retrieveAllExchange(exchangeList);
+		testOutput = String.format("%-10d %-30s %-10.2f\n", 1, "Exchange 1", 10.00);
+		testOutput += String.format("%-10d %-30s %-10.2f\n", 2, "Exchange 2", 20.00);
+
+		assertEquals("Test that ViewAllExchangelist", testOutput, allExchange);
+	}
+	
+	@Test
+	public void viewAllRefundTest() {
+		//=============================VIEW ALL REFUND===========================================
+		// Test if Item list is not null but empty -boundary
+		assertNotNull("Test if there is valid Refund arraylist to retrieve item", refundList);
+						
+		//test if the list of Refund retrieved from the SourceCentre is empty - boundary
+		String allRefund = C206_CaseStudy.retrieveAllRefund(refundList);
+		String testOutput = "";
+		assertEquals("Check that ViewAllRefundlist", testOutput, allRefund);
+		
+		//Given an empty list, after adding 2 items, test if the size of the list is 2 - normal
+		C206_CaseStudy.addRefund(refundList, re1);
+		C206_CaseStudy.addRefund(refundList, re2);
+		assertEquals("Test that Refund arraylist size is 2", 2, refundList.size());
+						
+		//test if the expected output string same as the list of Refund retrieved from the SourceCentre	
+		allRefund = C206_CaseStudy.retrieveAllRefund(refundList);
+		testOutput = String.format("%-10d %-30s %-10.2f\n", 3, "Refund 1", 30.00);
+		testOutput += String.format("%-10d %-30s %-10.2f\n", 4, "Refund 2", 40.00);
+
+		assertEquals("Test that ViewAllRefundlist", testOutput, allRefund);
+	}
+	
+	@Test
+	public void archivingExchangeTest() {
+		//==============================ARCHIVING EXCHANGE=======================================
+		//boundary
+		assertNotNull("Test if there is valid Exchange arraylist to add to", exchangeList);
+		C206_CaseStudy.addExchange(exchangeList, ex1);
+		//error
+		Boolean isArchived = C206_CaseStudy.doArchiveExchange(exchangeList, 1);
+		assertTrue("Test if Exchange 1 is archived - true", isArchived);		
+		//normal
+		C206_CaseStudy.addExchange(exchangeList, ex2);
+		isArchived = C206_CaseStudy.doArchiveExchange(exchangeList, 2);
+		assertTrue("Test if Exchange 2 is archived - true", isArchived);
+		//error
+		isArchived = C206_CaseStudy.doArchiveExchange(exchangeList, 3);
+		assertFalse("Test if non-existent Exchange 3 is archived - false?", isArchived);
 	}
 
 }
