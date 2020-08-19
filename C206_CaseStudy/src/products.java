9e83e5334c877579e4ebd1cd09289d72baf3b258
Mannmeet Singh <19038833@myrp.edu.sg>
2020-08-19 23:30:57 +0800

    Updated by Mannmeet

---
+++ C206_CaseStudy/src/products.java
@@ -0,0 +1,34 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19038833, 19 Aug 2020 10:08:38 pm
+ */
+
+/**
+ * @author 19038833
+ *
+ */
+public class products {
+	private String name;
+	
+	public products(String name) {
+		super();
+		this.name=name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String setName() {
+		this.name=name;
+	}
+	
+	public String toString() {
+		String output = String.format("%s", name);
+		return output;
+	}
+
+}
