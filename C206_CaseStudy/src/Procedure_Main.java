63f03678a561b444200bb9c2182226834f0e7c98
19016502 <19016502@Kitty.mshome.net>
2020-08-19 10:41:40 +0800

    Procedure codes(Elysha)

---
+++ C206_CaseStudy/src/Procedure_Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Procedure_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Refund> refundList = new ArrayList<Refund>();
+		ArrayList<Exchange> exchangeList = new ArrayList<Exchange>();
+		
+		
+
+	}
+
+}
